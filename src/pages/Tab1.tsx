dd515698a512808aaa34440f2375c1f136033b7c
aaadriana <adriana@stanciu.uk>
2021-11-14 17:35:18 +0000

    added bmi functionality on tab1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,73 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useRef, useState } from 'react';
+import {
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonPage,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import './Tab1.css';
+import '@ionic/react/css/core.css';
+
+import BmiControls from '../components/BmiControls';
+import BmiResult from '../components/BmiResult';
 
 const Tab1: React.FC = () => {
+  const [calculatedBmi, setCalculatedBmi] = useState<number>();
+
+  const weightInputRef = useRef<HTMLIonInputElement>(null);
+  const heightInputRef = useRef<HTMLIonInputElement>(null);
+
+  const calculateBMI = () => {
+    const enteredWeight = weightInputRef.current!.value;
+    const enteredHeight = heightInputRef.current!.value;
+
+    if (!enteredWeight || !enteredHeight) {
+      return;
+    }
+    const bmi = +enteredWeight / (+enteredHeight * +enteredHeight);
+    setCalculatedBmi(bmi);
+  };
+
+  const resetInputs = () => {
+    weightInputRef.current!.value = '';
+    heightInputRef.current!.value = '';
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>BMI Calculator</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent fullscreen className="ion-padding">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Your Height</IonLabel>
+                <IonInput ref={heightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Your Weight</IonLabel>
+                <IonInput ref={weightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <BmiControls onCalculate={calculateBMI} onReset={resetInputs} />
+          {calculatedBmi && <BmiResult result={calculatedBmi} />}
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
