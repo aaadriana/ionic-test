add560e025ead3cf5e4f26e645ae53a2b8b6c504
aaadriana <adriana@stanciu.uk>
2021-11-15 21:34:22 +0000

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDFzDGd_-1IXvwCc4XZCrL6BmD40fCa9Mw',
+  authDomain: 'ionictest-68fef.firebaseapp.com',
+  projectId: 'ionictest-68fef',
+  storageBucket: 'ionictest-68fef.appspot.com',
+  messagingSenderId: '695360359673',
+  appId: '1:695360359673:web:265e1478ccb5c6522e2628',
+  measurementId: 'G-F7BTXB78PN',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
